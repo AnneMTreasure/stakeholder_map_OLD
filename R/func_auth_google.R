a02d44a8538aba15b90bb1a5b127003b24ef5ec9
AnneMTreasure <AnneMTreasure@users.noreply.github.com>
2021-10-29 13:24:55 +0200

    Update func_auth_google.R

--- R/func_auth_google.R
+++ R/func_auth_google.R
@@ -1,4 +1,4 @@
-#' Authenticate Google Credentials
+#' Authenticate Google Credentials (sourced from https://github.com/jdtrat/tokencodr-google-demo)
 #'
 #' @param service The name of the service used to encrypt a file with [tokencodr::encrypt_token()]
 #' @param token_path Path to encrypted json file, the `destination` field of [tokencodr::encrypt_token()].
@@ -9,8 +9,4 @@ auth_google <- function(email, service, token_path) {
   googlesheets4::gs4_auth(email = "*@talarify.co.za", path = tokencodr::decrypt_token(service = service,
                                                           path = token_path,
                                                           complete = TRUE))
-  
-#  googledrive::drive_auth(path = tokencodr::decrypt_token(service = service,
-#                                                          path = token_path,
-#                                                          complete = TRUE))
 }
